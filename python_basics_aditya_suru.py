f8f21d34c3efdfb0524e0c4f1a4705fad6ce67c2
AdityaSuru62 <adorableaditya3@gmail.com>
2026-04-15 02:20:43 +0530

    Add files via upload

---
+++ python_basics_aditya_suru.py
@@ -0,0 +1,12 @@
+Name = "Aditya"
+Age = 27
+Height = 175.0
+Student = False
+print(Name, Age, Height, Student)
+print("Name :", Name, "Age :", Age, "Height :", Height ,"Student :", Student)
+Age_in_Months = Age*12
+Age_in_Days = Age*365
+print(Age_in_Months)
+print(Age_in_Days)
+print(Age/7)
+print(Age**2)
